449e4123348da8d4b8ed87291754c6a1451174bb
feng <aaa18520520650@163.com>
2024-04-19 21:31:20 +0800

    push commit

--- src/test/java/com/li/lrms/GitTest.java
+++ src/test/java/com/li/lrms/GitTest.java
@@ -18,6 +18,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("hot-fix");
-        System.out.println("github");
+        System.out.println("github test");
     }
 }
