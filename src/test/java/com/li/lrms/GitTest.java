d88253e8b0dfa3bf807dd1b87b1af5790c5ca4d8
feng <aaa18520520650@163.com>
2024-04-19 20:44:43 +0800

    hot-fix commit 2

--- src/test/java/com/li/lrms/GitTest.java
+++ src/test/java/com/li/lrms/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("分支");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix");
     }
 }
