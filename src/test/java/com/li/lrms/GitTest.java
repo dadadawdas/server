47d006ea5cdb04ee5fd79b5d40784bc071029886
feng <130761866+dadadawdas@users.noreply.github.com>
2024-04-20 01:11:04 +0800

    Update GitTest.java

--- src/test/java/com/li/lrms/GitTest.java
+++ src/test/java/com/li/lrms/GitTest.java
@@ -19,5 +19,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix");
         System.out.println("github test");
+         System.out.println("github test");
     }
 }
