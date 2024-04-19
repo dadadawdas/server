4071ffbe12a11ea048d07c93109a075d375a8e92
feng <aaa18520520650@163.com>
2024-04-19 16:35:04 +0800

    second

---
+++ src/test/java/com/li/lrms/GitTest.java
@@ -0,0 +1,16 @@
+package com.li.lrms;
+
+import com.google.common.annotations.VisibleForTesting;
+
+/**
+ * ClassName: GitTest
+ * Package: com.li.lrms
+ * Description:
+ *
+ * @Author: Mr.zechaowei
+ * @Create: 2024/4/19 - 16:32
+ * @Version: v1.0
+ */
+
+public class GitTest {
+}
